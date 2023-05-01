95e37dafc4b8800db05f312a8c54f6a5b119948a
Burak Arslan <91011599+Arsln44@users.noreply.github.com>
2023-05-01 05:55:02 +0300

    Create crypto_data_info.py

---
+++ crypto_data_info.py
@@ -0,0 +1,39 @@
+import requests
+import json
+
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+headers = {
+    'Accepts': 'application/json',
+    'X-CMC_PRO_API_KEY': 'MY_API_KEY'
+}
+response = requests.get(url, headers=headers)
+
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Canlı Kripto Para Verileri")
+root.configure(bg="black")
+
+label = tk.Label(root, text="Kripto Para Verileri",fg="green", font=("Terminal", 34))
+label.pack(pady=10)
+label.configure(bg="black", fg="green")
+
+data = tk.Text(root, height=20, width=80,background="black",fg="green",font=("Terminal",12))
+data.pack(padx=20, pady=20)
+
+
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+headers = {
+    'Accepts': 'application/json',
+    'X-CMC_PRO_API_KEY': 'MY_API_KEY'
+}
+response = requests.get(url, headers=headers)
+response_json = json.loads(response.text)
+
+for currency in response_json['data']:
+    name = currency['name']
+    symbol = currency['symbol']
+    price = currency['quote']['USD']['price']
+    data.insert(tk.END, f"{name} ({symbol}): ${price}\n\n")
+
+root.mainloop()
