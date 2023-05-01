1c19ea307da5d0836fbae0be5c7c6288839a5be1
Burak Arslan <91011599+Arsln44@users.noreply.github.com>
2023-05-01 16:20:09 +0300

    Update crypto_data_info.py

--- crypto_data_info.py
+++ crypto_data_info.py
@@ -1,7 +1,7 @@
 import requests
 import json
-
 import tkinter as tk
+import time
 
 root = tk.Tk()
 root.title("Canlı Kripto Para Verileri")
@@ -18,15 +18,19 @@ data.pack(padx=20, pady=20)
 url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
 headers = {
     'Accepts': 'application/json',
-    'X-CMC_PRO_API_KEY': 'MY_API_KEY'
+    'X-CMC_PRO_API_KEY': '37ec0d2d-88b3-4bd0-830c-e30f173b3706'
 }
-response = requests.get(url, headers=headers)
-response_json = json.loads(response.text)
+def update_data():
 
-for currency in response_json['data']:
-    name = currency['name']
-    symbol = currency['symbol']
-    price = currency['quote']['USD']['price']
-    data.insert(tk.END, f"{name} ({symbol}): ${price}\n\n")
+    response = requests.get(url, headers=headers)
+    response_json = json.loads(response.text)
 
+    data.delete("1.0",tk.END)
+    for currency in response_json['data']:
+        name = currency['name']
+        symbol = currency['symbol']
+        price = currency['quote']['USD']['price']
+        data.insert(tk.END, f"{name} ({symbol}): ${price}\n\n")
+    root.after(3000,update_data)
+update_data()
 root.mainloop()
